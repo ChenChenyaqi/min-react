e790dd2b92a9fc6e33dc82cb38e48d2fd9a6ad9e
chenyaqi <henchenyaqi319@gmail.com>
2024-04-01 22:24:48 +0800

    feature: fiber架构&统一提交

--- core/React.ts
+++ core/React.ts
@@ -24,27 +24,103 @@ function createElement(
   }
 }
 
-function render(el: any, container: HTMLElement) {
-  const { type, props } = el
-  const dom =
-    type === "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(type)
+function createDom(type) {
+  return type === "TEXT_ELEMENT"
+    ? document.createTextNode("")
+    : document.createElement(type)
+}
 
-  if (props) {
-    Object.keys(props).forEach((key) => {
-      if (key !== "children") {
-        dom[key] = props[key]
-      }
-    })
+function updateProps(dom, props) {
+  Object.keys(props).forEach((key) => {
+    if (key !== "children") {
+      dom[key] = props[key]
+    }
+  })
+}
 
-    const children = props.children
-    children?.forEach((child: any) => {
-      render(child, dom)
-    })
+function initChildren(fiber) {
+  // 转换链表
+  const children = fiber.props.children
+  let preChild = null as any
+  children?.forEach((child: any, index: number) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      parent: fiber,
+      child: null,
+      sibling: null,
+      dom: null,
+    }
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      preChild.sibling = newFiber
+    }
+
+    preChild = newFiber
+  })
+}
+
+let nextWorkOfUnit = null as any
+let root
+function workLoop(deadline: IdleDeadline) {
+  let shouldYield = false
+  while (!shouldYield && nextWorkOfUnit) {
+    nextWorkOfUnit = performWorkOfUnit(nextWorkOfUnit)
+
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  if (!nextWorkOfUnit && root) {
+    // 统一提交
+    commitRoot()
+    root = null
+  }
+  requestIdleCallback(workLoop)
+}
+
+function performWorkOfUnit(fiber) {
+  if (!fiber.dom) {
+    const dom = (fiber.dom = createDom(fiber.type))
   }
 
-  container.append(dom)
+  updateProps(fiber.dom, fiber.props)
+
+  initChildren(fiber)
+
+  // 返回下一个要执行的任务
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  if (fiber.sibling) {
+    return fiber.sibling
+  }
+
+  return fiber.parent?.sibling
+}
+
+function commitRoot() {
+  commitWork(root.child)
+}
+
+function commitWork(fiber) {
+  if (!fiber) {
+    return
+  }
+  fiber.parent.dom.append(fiber.dom)
+  commitWork(fiber.child)
+  commitWork(fiber.sibling)
+}
+
+function render(el: any, container: HTMLElement) {
+  root = nextWorkOfUnit = {
+    dom: container,
+    props: {
+      children: [el],
+    },
+  }
+
+  requestIdleCallback(workLoop)
 }
 
 const React = {
